ee7f405ce026c3be4cef7009a501a8e96b882cf0
林佩瑤 <rikkubook@gmail.com>
2022-02-20 23:00:53 +0800

    [添加 todolis api 新刪修]

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ const server = http.createServer((req, res) => {
 
     let body =""
     req.on('data', chunk =>{
-        console.log(chunk)
         body+=chunk;
     })
 
@@ -36,7 +35,7 @@ const server = http.createServer((req, res) => {
                 if(title !== undefined){
                     const todo = { // 模擬打進資料庫的內容
                         "title": title,
-                        "uuid": uuidv4()
+                        "id": uuidv4()
                     }
                     console.log(todo);
                     todos.push(todo);
@@ -52,9 +51,51 @@ const server = http.createServer((req, res) => {
             }catch(error){
                 errorHandle(res); // 也代表要回傳給哪個user
             }
-
         })
-
+    }else if(req.url === '/todos' && req.method === 'DELETE'){ // 刪除全部
+        todos.length = 0
+        res.writeHead(200, headers); 
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+            "delete": "delete"
+        }));
+        res.end();
+    }else if(req.url.startsWith("/todos/")  && req.method === 'DELETE'){ // 刪除一筆
+        const id = req.url.split('/').pop();
+        const index = todos.findIndex(item => item.id === id); //找索引
+        if(index !== -1){  // 有索引
+            todos.splice(index,1) // 刪除此筆
+            res.writeHead(200, headers); 
+            res.write(JSON.stringify({
+                "status": "success",
+                "data": todos,
+            }));
+            res.end();
+        }else{
+            errorHandle(res); 
+        }
+    }else if(req.url.startsWith("/todos/")  && req.method === 'PATCH'){ // 修改一筆
+        req.on('end',()=>{ // 打進來的資料 確保組好了 body的資料
+            try{
+                const title = JSON.parse(body).title;
+                const id = req.url.split('/').pop();
+                const index = todos.findIndex(item => item.id === id); //找索引
+                if(title !== undefined &&  index !== -1){ // 有title 且 有值
+                    todos[index].title = title //寫入覆蓋
+                    res.writeHead(200, headers);
+                    res.write(JSON.stringify({
+                        "status": "success",
+                        "data": todos
+                    }));
+                    res.end();
+                }else{
+                    errorHandle(res);
+                }
+            }catch(error){
+                errorHandle(res); // 也代表要回傳給哪個user
+            }
+        })
     }else if(req.method === 'OPTIONS'){
         res.writeHead(200, headers);
         res.end();
@@ -66,7 +107,6 @@ const server = http.createServer((req, res) => {
         }));
         res.end();
     }
-
 })
 
 server.listen(8080,()=>{
