8593c557cdd8605fafee791667169c345f042631
林佩瑤 <rikkubook@gmail.com>
2022-02-26 15:05:06 +0800

    [優化 判斷API]

--- app.js
+++ app.js
@@ -2,110 +2,155 @@ const http = require('http');
 const { v4: uuidv4 } = require('uuid'); 
 const errorHandle = require('./errorHandle');
 
-const todos = [
+const todoList = [
     //{"title": "今天要刷牙", "id": uuidv4() } //假資料，重啟動伺服器時會產生
 ];
 
 const server = http.createServer((req, res) => {
-    const headers = { 
+    const Header =(number) =>  res.writeHead(number, {
         'Content-Type': 'application/json' ,
         'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
         'Access-Control-Allow-Origin':  '*',
         'Access-Control-Allow-Methods': 'POST, GET, OPTIONS,DELETE'
-    }
+       }); // 預設文字檔案
 
     let body =""
     req.on('data', chunk =>{
         body+=chunk;
     })
 
-
-    if(req.url === '/todos' && req.method === 'GET'){ // 取得所有
-        res.writeHead(200, headers); 
-        res.write(JSON.stringify({
-            "status": "success",
-            "data": todos
-        }));
-        res.end();
-    } else if(req.url === '/todos' && req.method === 'POST'){ // 新增一比
-        req.on('end',()=>{ // 確保組好了 body的資料
-            try{
-                const title = JSON.parse(body).title;
-
-                if(title !== undefined){
-                    const todo = { // 模擬打進資料庫的內容
-                        "title": title,
-                        "id": uuidv4()
+    if(req.url==='/todos'){
+        let method =  req.method
+        switch(method){
+            case 'GET': 
+                Header(200) 
+                res.write(JSON.stringify({
+                    "status": "success",
+                    "data": todoList
+                }));
+                res.end()
+                break;
+            case 'POST':
+                req.on('end',()=>{
+                    try{
+                        const title = JSON.parse(body).title;
+                        if(title !== undefined){ // 沒有title
+                            const todo = { // 模擬打進資料庫的內容
+                                "title": title,
+                                "id": uuidv4()
+                            }
+                            todoList.push(todo);
+                            Header(200) 
+                            res.write(JSON.stringify({
+                                "status": "success",
+                                "data": todoList
+                            }));
+                            res.end();
+                        }else{
+                            Header(400) 
+                            res.write(JSON.stringify({
+                                "status": "false",
+                                "data": "沒有 title"
+                            }));
+                            res.end();
+                        }
+                    }catch(error){ // 非JSON格式
+                        console.log("Error");
+                        console.log(error);
+                        Header(400) 
+                        res.write(JSON.stringify({
+                            "status": "false",
+                            "data": "欄位未填寫正確，或無此 todo id"
+                        }));
+                        res.end();
                     }
-                    console.log(todo);
-                    todos.push(todo);
-                    res.writeHead(200, headers);
-                    res.write(JSON.stringify({
-                        "status": "success",
-                        "data": todos
-                    }));
-                    res.end();
-                }else{
-                    errorHandle(res);
-                }
-            }catch(error){
-                errorHandle(res); // 也代表要回傳給哪個user
-            }
-        })
-    }else if(req.url === '/todos' && req.method === 'DELETE'){ // 刪除全部
-        todos.length = 0
-        res.writeHead(200, headers); 
-        res.write(JSON.stringify({
-            "status": "success",
-            "data": todos,
-            "delete": "delete"
-        }));
-        res.end();
-    }else if(req.url.startsWith("/todos/")  && req.method === 'DELETE'){ // 刪除一筆
-        const id = req.url.split('/').pop();
-        const index = todos.findIndex(item => item.id === id); //找索引
-        if(index !== -1){  // 有索引
-            todos.splice(index,1) // 刪除此筆
-            res.writeHead(200, headers); 
-            res.write(JSON.stringify({
-                "status": "success",
-                "data": todos,
-            }));
-            res.end();
-        }else{
-            errorHandle(res); 
+                })
+                break;
+            case 'DELETE':
+                todoList.length = 0;
+                Header(200) 
+                res.write(JSON.stringify({
+                    "status": "success",
+                    "data": todoList,
+                    "message": "全部刪除成功"
+                }));
+                res.end();
+            break;
+            default:
+                console.log(`Sorry, we are out of ${method}.`);
         }
-    }else if(req.url.startsWith("/todos/")  && req.method === 'PATCH'){ // 修改一筆
-        req.on('end',()=>{ // 打進來的資料 確保組好了 body的資料
-            try{
-                const title = JSON.parse(body).title;
+    }else if(req.url.startsWith('/todos/') ){
+        let method =  req.method
+        switch(method){
+            case 'DELETE':
                 const id = req.url.split('/').pop();
-                const index = todos.findIndex(item => item.id === id); //找索引
-                if(title !== undefined &&  index !== -1){ // 有title 且 有值
-                    todos[index].title = title //寫入覆蓋
-                    res.writeHead(200, headers);
+                const index = todoList.findIndex(item => item.id === id); //找索引
+                if(index !== -1){
+                    todoList.splice(index, 1);
+                    Header(200) 
                     res.write(JSON.stringify({
                         "status": "success",
-                        "data": todos
+                        "data": todoList,
                     }));
                     res.end();
                 }else{
-                    errorHandle(res);
+                    Header(404) 
+                    res.write(JSON.stringify({
+                        "status": "false",
+                        "data": "刪除"
+                    }));
+                    res.end();
                 }
-            }catch(error){
-                errorHandle(res); // 也代表要回傳給哪個user
-            }
-        })
+                break;
+            case 'PATCH':
+                req.on('end',()=>{
+                    try{
+                        const title = JSON.parse(body).title;
+                        const id = req.url.split('/').pop();
+                        const index = todoList.findIndex(item => item.id === id); //找索引
+        
+                        if(title !== undefined &&  index !== -1){ //
+                            todoList[index].title = title
+                            Header(200) 
+                            res.write(JSON.stringify({
+                                "status": "success",
+                                "data": todoList
+                            }));
+                            res.end();
+                        }else{
+                            Header(400) 
+                            res.write(JSON.stringify({
+                                "status": "false",
+                                "data": "沒有 title"
+                            }));
+                            res.end();
+                        }
+                    }catch(error){ // 非JSON格式
+                        console.log("Error");
+                        console.log(error);
+                        Header(400) 
+                        res.write(JSON.stringify({
+                            "status": "false",
+                            "data": "欄位未填寫正確，或無此 todo id"
+                        }));
+                        res.end();
+                    }
+                })
+                break;
+            default:
+                console.log(`Sorry, we are out of ${method}.`);
+        }
     }else if(req.method === 'OPTIONS'){
-        res.writeHead(200, headers);
-        res.end();
-    }else{
-        res.writeHead(404, headers); // 預設文字檔案
+        Header(200) 
+        console.log('經過OPTIONS');
+        res.end()
+    }else {
+        Header(404) 
         res.write(JSON.stringify({
             "status": "false",
-            "message": "無此網站路由"
+            "data": "沒有此路由"
         }));
-        res.end();
+        res.end()
     }
 })
 
